9c75c8e951d85db15577e81219c1b2289c6e4926
Xavier Sala <utrescu@gmail.com>
2013-12-04 15:16:41 +0100

    Modifico perquè no m'ha actualitzat el "merge"

--- Marcianets/src/main/java/net/xaviersala/marcianets/BalaEnemiga.java
+++ Marcianets/src/main/java/net/xaviersala/marcianets/BalaEnemiga.java
@@ -16,7 +16,6 @@ public class BalaEnemiga extends Bala {
      */
     public BalaEnemiga(final Image img, final double x, final double y) {
         super(img, x, y, Direccio.AVALL);
-        // TODO Auto-generated constructor stub
     }
 
 }
